9c2242fe513b7ab08d65f248feb596cb8264983a
prototypedave <popupgfj@gmail.com>
2025-02-22 12:16:02 +0300

    Feat: Add scrapping for individual match details

--- scrapping.py
+++ scrapping.py
@@ -2,6 +2,31 @@ from playwright.sync_api import sync_playwright
 import pandas as pd
 import time
 
+def scrape_match_details(browser, href):
+    """
+        Gets data for individual games
+    """
+    # open a new page
+    match_page = browser.new_page()
+    match_page.goto(href, timeout=60000)
+    match_page.wait_for_selector(".duelParticipant", timeout=10000)
+
+    home_team = match_page.locator(".duelParticipant__home .participant__participantName a").first.inner_text().strip()
+    away_team = match_page.locator(".duelParticipant__away .participant__participantName a").first.inner_text().strip()
+    score = match_page.locator(".detailScore__wrapper").inner_text().strip()
+    time = match_page.locator(".duelParticipant__startTime div").inner_text().strip()
+
+    match_data = {
+        'home': home_team,
+        'away': away_team,
+        'score': score,
+        'time': time
+    }
+    print(match_data)
+
+    match_page.close()
+
+
 def scrape_flashscore():
     """
         Get flashscore matches for the day
@@ -21,7 +46,14 @@ def scrape_flashscore():
         for event in events:
             match_link = event.locator("a").first
             href = match_link.get_attribute("href")
-            print(href)
 
+            # get match details
+            scrape_match_details(browser=browser, href=href)
+            
         browser.close()
+
+
+
+
+
 scrape_flashscore()
