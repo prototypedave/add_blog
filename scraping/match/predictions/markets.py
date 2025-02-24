64e4c5b7b69df5e5949ac593ec949dbbe122bb9b
prototypedave <popupgfj@gmail.com>
2025-02-24 10:45:20 +0300

    Feat: Groq prediction based on injury

--- scraping/match/predictions/markets.py
+++ scraping/match/predictions/markets.py
@@ -23,14 +23,44 @@ def team_form(home, away):
         stop=None,
     )
 
-    response_text = ""  # Initialize an empty string to store the response
+    response_text = ""  
 
     for chunk in completion:
         if chunk.choices[0].delta.content:
-            response_text += chunk.choices[0].delta.content  # Append content to the variable
+            response_text += chunk.choices[0].delta.content  
 
     try:
-        response_json = json.loads(response_text.strip())  # Convert the accumulated text to JSON
+        response_json = json.loads(response_text.strip())  
+        return response_json
+    except json.JSONDecodeError:
+        return {}
+
+
+def injury_predictions(home, away):
+    completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": f"You are a top sports betting expert in the world with almost 100% win rate. You are tasked to analyze teams latest injuries of key players, suspension reports and any missing key player prior to the given match that is to be played and provide an analysis from it on how the report can affect different betting markets, which to avoid and which rises as an opportunity for betting to. Return only json objects with this keys : 'markets to avoid' , 'opportunity markets' and 'reason'. Note only return objects and nothing else.\nProvided: {home} v {away}"
+            },
+            
+        ],
+        temperature=1,
+        max_completion_tokens=1024,
+        top_p=1,
+        stream=True,
+        stop=None,
+    )
+
+    response_text = ""  
+
+    for chunk in completion:
+        if chunk.choices[0].delta.content:
+            response_text += chunk.choices[0].delta.content  
+
+    try:
+        response_json = json.loads(response_text.strip())  
         return response_json
     except json.JSONDecodeError:
         return {}
--- scraping/match/predictions/predict.py
+++ scraping/match/predictions/predict.py
@@ -1,6 +1,6 @@
-from .markets import team_form
+from .markets import team_form, injury_predictions
 
 def get_prediction(data):
     pred_form = team_form(data.get('home'), data.get('away'))
-    print(pred_form)
-
+    pred_injury = injury_predictions(data.get('home'), data.get('away'))
+    print(pred_injury)
