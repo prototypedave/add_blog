eeb781b2d2fdfcb260bf1df1f12d76829b7f7e20
prototypedave <popupgfj@gmail.com>
2025-02-24 12:49:06 +0300

    New: Tactical Predictions

--- scraping/match/predictions/predict.py
+++ scraping/match/predictions/predict.py
@@ -1,7 +1,9 @@
 from .markets import team_form
 from .injury import injury_predictions
+from .tactical import tactical_prediction
 
 def get_prediction(data):
     pred_form = team_form(data.get('home'), data.get('away'))
     pred_injury = injury_predictions(data.get('home'), data.get('away'))
-    print(pred_injury)
+    pred_tactical = tactical_prediction(data.get('home'), data.get('away'))
+    print(pred_tactical)
