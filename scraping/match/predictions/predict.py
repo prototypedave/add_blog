460f001e12f52be0bcc204331ea661d168045cf7
prototypedave <popupgfj@gmail.com>
2025-02-24 12:53:54 +0300

    New: Match motivation prediction

--- scraping/match/predictions/predict.py
+++ scraping/match/predictions/predict.py
@@ -1,9 +1,11 @@
 from .markets import team_form
 from .injury import injury_predictions
 from .tactical import tactical_prediction
+from .motivation import match_motivation
 
 def get_prediction(data):
     pred_form = team_form(data.get('home'), data.get('away'))
     pred_injury = injury_predictions(data.get('home'), data.get('away'))
     pred_tactical = tactical_prediction(data.get('home'), data.get('away'))
-    print(pred_tactical)
+    pred_motivation = match_motivation(data.get('home'), data.get('away'))
+    print(pred_motivation)
