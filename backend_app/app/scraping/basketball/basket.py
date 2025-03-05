21f3e6ca6a9348ca8e781fbd7bbae8268102423c
prototypedave <popupgfj@gmail.com>
2025-03-05 23:43:06 +0300

    API: Fix basketball API to save accurate data

--- backend_app/app/scraping/basketball/basket.py
+++ backend_app/app/scraping/basketball/basket.py
@@ -1,7 +1,10 @@
 from playwright.sync_api import Page
 from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
 from app.scraping.events import get_events
-from app.scraping.match_details import match_details
+from app.scraping.match_details import match_details, is_record_existing, update_score
+from app.models.basketball import BasketPrediction
+from datetime import datetime
+from .h2h import get_h2h
 
 """
     Gets flashscore data for basketball
@@ -10,4 +13,27 @@ def scrape_basketball(page: Page, db):
     events = get_events(page=page, href="https://www.flashscore.co.ke/basketball/")
     for link in events:
         match = match_details(page, link)
-        print(match)
+
+        home = match['home']
+        away = match['away']
+        time = match['time']
+        score = match['score']
+
+        if not is_record_existing(db=db, table=BasketPrediction, home=home, away=away, time=time):
+            # Check if a match is viable for prediction
+            if datetime.strptime(time, "%I:%M %p, %B %d, %Y") > datetime.now():
+                prediction = get_h2h(page, link[:link.rfind('#')] + "#/h2h", home, away)
+                if prediction:
+                    new_pred = BasketPrediction(
+                        league=match['country'],
+                        home_team=home,
+                        away_team=away,
+                        result=score,
+                        time=time
+                    )
+                    new_pred.set_prediction(prediction)
+                    db.session.add(new_pred)
+                    db.session.commit()
+                    print(f"Home team: {home}, Prediction: {prediction}")
+        # Update score
+        update_score(db=db, table=BasketPrediction, home=home, away=away, time=time, score=score)
--- backend_app/app/scraping/basketball/h2h.py
+++ backend_app/app/scraping/basketball/h2h.py
@@ -1,4 +1,9 @@
 from playwright.sync_api import Page
+from app.scraping.h2h import get_h2h_details
+from app.algorithms.basket import predict_basketball
+from .stats.over import total_over
+from .stats.handicap import get_handicap_value
+from .stats.win import get_team_win
 
 """
     Returns prediction based on head-to-head analysis
@@ -7,3 +12,12 @@ def get_h2h(page: Page, link: str, home: str, away: str):
     ovr_link = link + "/overall"
     home_link = link + "/home"
     away_link = link + "/away"
+
+    ovr_results  = get_h2h_details(page, ovr_link)
+    home_results = get_h2h_details(page, home_link)
+    away_results = get_h2h_details(page, away_link)
+    
+    total = total_over(ovr_results, home_results, away_results, home, away)
+    hc = get_handicap_value(ovr_results)
+    win = get_team_win(ovr_results, home, away)
+    return predict_basketball(ovr_results, total, hc, win)
