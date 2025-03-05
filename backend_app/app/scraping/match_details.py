f5eb2541c068830ebb41e90d1f74727e7f58ee10
prototypedave <popupgfj@gmail.com>
2025-03-05 23:01:17 +0300

    Feat: Keep on updating results as soon as there is any

--- backend_app/app/scraping/match_details.py
+++ backend_app/app/scraping/match_details.py
@@ -18,3 +18,29 @@ def match_details(page: Page, href: str):
         'time': time,
         'country': country
     }
+
+
+"""
+    Checks if a match is already saved in DB
+"""
+def is_record_existing(db, table, home, away, time):
+    if db.session.query(table).filter_by(
+        home_team=home,
+        away_team=away,
+        time=time
+    ).first():
+        return True
+    return False
+    
+
+"""
+    Checks if we have current results is already saved in DB
+"""
+def update_score(db, table, home, away, time, score):
+    record = db.session.query(table).filter_by(
+        home_team=home,
+        away_team=away,
+        time=time,
+    ).first()
+    record.result = score
+    db.session.commit()
