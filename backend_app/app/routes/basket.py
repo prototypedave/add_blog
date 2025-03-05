11437e7b00432bdec238791b9aa72a435c1670ee
prototypedave <popupgfj@gmail.com>
2025-03-05 20:51:47 +0300

    API: basketball API

---
+++ backend_app/app/routes/basket.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify
+from datetime import datetime
+from storage.database import db
+from models.basketball import BasketPrediction
+
+basket_bp = Blueprint('basket_bp', __name__)
+
+@basket_bp.route('/basketball/general', methods=['GET'])
+def get_basket():
+    """
+        API Endpoint for basketball predictions
+    """
+    today = datetime.now().date()
+    predictions = BasketPrediction.query.filter(
+        db.func.date(BasketPrediction.created_at) == today
+    ).all()
+
+    if not predictions:
+        most_recent_date = db.session.query(
+            db.func.date(BasketPrediction.created_at)
+        ).order_by(db.func.date(BasketPrediction.created_at).desc()).first()
+
+        if most_recent_date:
+            predictions = BasketPrediction.query.filter(
+                db.func.date(BasketPrediction.created_at) == most_recent_date[0]
+            ).all()
+    response_data = {}
+
+    for match in predictions:
+        if match.league not in response_data:
+            response_data[match.league] = []
+        response_data[match.league].append(match.to_dict())
+
+    final_response = [{"league": league, "matches": matches} for league, matches in response_data.items()]
+    return jsonify(final_response)
